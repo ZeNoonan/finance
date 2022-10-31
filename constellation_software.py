667a72294d4372b289dbd8d425b3c2c207a9f890
ZeNoonan <darragh.noonan@gmail.com>
2022-10-31 09:39:16 +0000

    first commit

---
+++ constellation_software.py
@@ -0,0 +1,168 @@
+import streamlit as st
+
+with st.expander('April 2018 letter'):
+    st.write("Some of our businesses serve shrinking\
+    verticals or those that are otherwise troubled, so we don’t necessarily expect strong organic growth from\
+    them. We do expect each business unit (“BU”) to provide constantly evolving software and systems that\
+    help their clients refine and strengthen their businesses, even in the face of industry headwinds.")
+    st.write("The growth in FCF per share metric\
+    isn’t very sensitive to debt and cash, but right now our net cash is not significant. Jamal’s preferred\
+    measure of our progress is the growth in ANI per share. It is less volatile than FCF per share (because the\
+    variance in net working capital largely washes out over the long run), and it does adjust for the portion of\
+    ANI that should accrue to minority shareholders.")
+    st.write("If I had to stake my reputation on one of those as the best proxy for the annual increase in intrinsic value, I\
+    would go with the increase in FCF per share. Jamal may yet argue me around to the growth in ANI per\
+    share. Keep in mind that you don’t need to use any single metric to judge our performance. Enterprising\
+    long-term investors will look at many metrics.")
+    st.write("I am suspicious of 'vision'. Long-term studies suggest that the underlying predictions or assumptions for\
+    visions are nearly always impractically vague or outright wrong (see Tetlock’s “Superforecasting”). I am\
+    not much happier with the term 'mission'. It feels too heavily freighted with overtones of hierarchy and\
+    unquestioning compliance. I prefer to talk about Constellation's objective. Our objective is to be a great\
+    perpetual owner of VMS businesses. We like VMS businesses because they are asset-light, have robust\
+    moats, and attract the sort of managers and employees with whom we enjoy working. ")
+    st.write("One day Constellation may find that VMS businesses are too expensive to rationally acquire. If that\
+    happens, I hope we'll have had the foresight and luck to find some other high ROE non-VMS businesses\
+    in which to invest at attractive prices. I am already casting about for such opportunities. If we don’t find\
+    attractive sectors in which to invest, then we’ll return our FCF to our investors. ")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+
+
+
+with st.expander('April 2017 letter'):
+
+    st.write("2017: In my letter last year, I explained \
+    that we sometimes buy shrinking businesses, and despite the shrinkage, we still expect to generate good \
+    returns on those investments. Growing businesses are more attractive to us, but we can't always acquire \
+    enough growing businesses at reasonable prices to invest all of our FCF. Our 'next best' use of capital is \
+    acquiring shrinking VMS businesses which still meet or exceed our hurdle rate")
+
+    st.write("The internal execution issues upon which we currently focus are: Maintaining investment discipline,\
+    avoiding overhead creep, and increasing our investment in growth, both organic and acquired.")
+
+    st.write("An investment only becomes a lesson if we diligently track its post-acquisition performance and take the \
+    time to analyse the outcome while the investment is still fresh in everyone’s mind. We have a process for\
+    this that we call a post-acquisition review, or “PAR”. We try to schedule the PAR’s about a year after the\
+    initial investment. The PAR’s originated as a head office led process approximately four years ago. Just\
+    over a year ago, we started delegating them down to the Operating Groups.")
+
+    st.write("One of the useful things that head office can do, is pilot new processes and champion new ideas. If the \
+    ideas add enough value to the BU’s and Operating Groups, and they choose to maintain them, then I’m\
+    delighted. Nevertheless, I think all processes should be periodically re-examined for their cost and\
+    benefit. An ad-hoc analysis done to understand a problem or opportunity is more likely to translate into\
+    action than a quarterly report that gets generated because “we’ve always done it that way”. The former\
+    requires curiosity and intelligence, the latter bureaucracy and compliance. If the Operating Groups can\
+    learn from their acquisitions by some less burdensome method than PAR’s, I’m all for it.")
+
+    st.write("Overhead creep is a short-term concern of mine and the BU Managers")
+    st.write("The only way we’ve been able to consistently stifle overhead growth at head office is to arbitrarily limit\
+    headcount additions. That has allowed us to reduce the head office burden from 3.0% of Net Revenue in\
+    2004, to 0.5% last year. We hope it will be lower in 2017.")
+
+    st.write("I have struggled to find a less arbitrary means of appropriately sizing overheads. A couple of years ago,\
+    our head office tax folks seemed to have an insatiable appetite for increased headcount. I couldn’t argue\
+    with their justification, but I asked them to start billing the Operating Groups for the incremental services,\
+    separate from our normal overhead allocation. There were two short-term results… our head office tax\
+    people hated billing the Operating Groups and justifying their bills, and one of the Operating Groups went\
+    off and hired their own tax person. The long-term result also pleased me: the head office tax people have\
+    stopped asking about hiring additional staff. Now, if I could just figure out how to stop them spending all\
+    that money with outside tax consultants…")
+
+    st.write("I believe that optimising organic growth investment is the single toughest management task in software.\
+    It requires a long-term orientation and an intimate understanding of customers and capabilities from our\
+    BU Managers")
+    st.write("Historically, organic growth has not been a struggle for our best BU Managers. When\
+    most of our current Operating Group Managers ran single BU’s, they had strong organic growth\
+    businesses. As those managers gave up their original BU management position to oversee a larger Group\
+    of BU’s (i.e. became Portfolio Managers), the organic growth of their original BU’s decreased and the\
+    profitability of those BU’s increased. Perhaps those trade-offs were rational and inevitable, and it was\
+    just a function of maturing verticals and higher market share. Nevertheless, once you’ve experienced\
+    higher organic growth with all of its ancillary benefits for employees and for the depth and radius of your\
+    business moat, the move towards higher profit and lower growth is much less satisfying. Across the\
+    board, our Operating Group Managers have organic growth as the primary objective for their BU\
+    Managers.")
+
+    st.write("CSI's strategy is to be a good owner of hundreds (and perhaps someday thousands) of growing\
+    autonomous small businesses that generate high returns on capital. Our strategy is unusual. Most CEO's\
+    of public companies would rather run a single big business - perhaps two or three big businesses, but\
+    rarely 200 businesses. They expect (or hope) to get above average returns on capital by pursuing economies of scale and \
+    by crushing or acquiring their smaller competition. 'We are #1 in this large and\
+    growing market' is their normal aspirational paradigm. It's also a formula with which shareholders,\
+    analysts and boards are comfortable. We recognise that economies of scale, centralised management and\
+    world class talent competing in large and growing markets can be a great business-building formula. But,\
+    it isn't what we do.")
+
+    st.write("We seek out vertical market software businesses where motivated small teams composed of good people,   \
+    can produce superior results in tiny markets. These markets are usually characterised by a gradually\
+    consolidating customer base, so partnering with the right clients, and helping them survive and prosper is\
+    an important part of our job. What we offer our BU Managers is autonomy, an environment that supports\
+    them in mastering vertical market software management skills, and the chance to build an enduring and\
+    competent team in a “human-scale” business.")
+
+
+
+    st.write("While we have developed some techniques and best practices for fostering organic growth, I think our\
+    most powerful tool is using human-scale BU’s.")
+    st.write("Priorities are clear, systems haven't had\
+    time to metastasise, rules are few, trust and communication are high, and the focus tends to be on how to\
+    increase the size of the pie, not how it gets divided. That's how I remember my favourite venture\
+    investments when I was a venture capitalist, and it's how I remember many of the early CSI acquisitions.\
+    That structure usually suffices until there are perhaps 30 to 40 people in the business.")
+
+    st.write("The larger a business gets, the more difficult it becomes to manage and the more policies, procedures,\
+    systems, rules and regulations are generated to handle the growing complexity. Talented people get\
+    frustrated, innovation suffers, and the focus shifts from customers and markets to internal communication,\
+    cost control, and rule enforcement. The quirky but talented rarely survive in this environment. A huge\
+    body of academic research confirms that complexity and co-ordination effort increase at a much faster\
+    rate than headcount in a growing organisation.")
+    
+    st.write("If the BU is small enough, and has a competent BU manager who has several years experience in the\
+    vertical, and good functional managers, then he/she will be able to cope with complexity for a while,\
+    making the right calls to optimise organic growth as the business grows. The challenge of running a BU\
+    of this size is human-scaled. As a BU becomes larger (by our standards, that’s greater than 100\
+    employees), I worry that even an extraordinarily brilliant and energetic manager, who has been in the\
+    vertical and the BU for a very long time, and is surrounded by a strong team that he/she has selected and\
+    trained and winnowed over many years, is going to struggle to steer the business to above industryaverage organic growth.")
+    
+    st.write("No one wants to admit that they’ve hit their limit. Some BU Managers lack the humility, some lack the\
+    courage, and most lack the time for reflection, to notice that their task is getting too large, and the\
+    sacrifices are getting too great.")
+
+    st.write("This dividing of larger BU’s into smaller units is rare, but not unknown, in other large companies. One of\
+    the HPC’s that we studied was Illinois Tool Works Inc. (“ITW”). It has hundreds of BU’s. We began\
+    following the company from afar in 2005.I’m\
+    sorry I didn’t reach out to some of the ITW employees and ex-employees until 2015. When I did talk\
+    with one of the senior managers, he said (I’m paraphrasing) “Something wonderful happens when you\
+    spin off a new business unit.” … “With a clean sheet of paper, the leader only takes those he needs. They\
+    set up in an open office with good communication and no overheads. They cover for each other. They\
+    leave all the bureaucracy and the crap behind”. I did record a couple of verbatim quotes from that\
+    conversation: 'Don't share sales, R&D, HR, etc. because the accountants never get the allocations right\
+    and the business units always treat the allocated costs as outside their control', and 'When you get big\
+    you lose entrepreneurship'")
+
+    st.write("I don’t want to give you the impression that the 'human-scale' BU idea is a universally accepted doctrine\
+    in our ranks. For that, I suspect we’d need more compelling data. However, we have been successfully\
+    experimenting with the concept for a long time.Harris has very successfully acquired multiple BU's in the same industry\
+    and run them independently rather than combining them into one BU. Both tactics forego obvious and\
+    easily obtainable benefits from economies of scale. We think we get something valuable when we\
+    constrain BU headcount, but it isn’t a panacea for all of our organic growth challenges.")
+    
+    st.write("This sort of career path obviously worked for our current Operating Group Managers, who all either came\
+    up through the ranks or joined us via an acquisition. I believe that attracting, developing, and keeping\
+    that sort of talent, is the internal execution issue that poses the greatest threat to our continued success.")
+    st.write("Some businesses get their unique advantage from government-granted monopolies, some from natural\
+        resources, some from large patent portfolios, and some from enormous fixed assets. CSI doesn’t have\
+        these advantages. Our employees, and the customer relationships that those employees have built and\
+        fostered over many years, provide our competitive advantage.")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
+    st.write("")
