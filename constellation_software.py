40aafadab261d759dd626f7b25fd62ba6b51a09f
ZeNoonan <darragh.noonan@gmail.com>
2022-10-31 16:57:26 +0000

    update

--- constellation_software.py
+++ constellation_software.py
@@ -24,14 +24,51 @@ with st.expander('April 2018 letter'):
     happens, I hope we'll have had the foresight and luck to find some other high ROE non-VMS businesses\
     in which to invest at attractive prices. I am already casting about for such opportunities. If we don’t find\
     attractive sectors in which to invest, then we’ll return our FCF to our investors. ")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
+
+with st.expander('Note to Shareholders regarding non-IFRS Measure'):
+    st.write("In my last Letter to Shareholders dated April 20, 2018, I wrote about the pros and cons of using the growth in\
+    Adjusted net income (“ANI”) per share versus cash flow from operations ('CFO') per share as indicators of the\
+    increase in CSI's economic value. Neither metric is ideal.\
+    Too often managers and shareholders unquestioningly accept ANI as the equivalent of earnings available to\
+    shareholders. Regulators require reconciliations of ANI to net income to highlight the difference, but these\
+    reconciliations often get short shrift. CFO doesn’t distinguish well between the return on capital and the return of capital.\
+    In CSI's reconciliation of ANI and Adjusted EBITA to net income the largest item is the amortisation of intangible\
+    assets. Clearly some of this amortisation represents an actual decrease in the economic value of the related\
+    underlying intangible assets. Our appreciating intangible assets have more than offset depreciating intangible assets\
+    to date, so I didn't fret about adding back the entire amount when calculating ANI. That will not always be the case.\
+    We have literally thousands of purchased products and the associated customer relationships. Many of these\
+    intangible assets are regularly maintained, enhanced and extended. A few deteriorate - some due to irrational\
+    competitors, some due to poor management and some due to shrinkage in the underlying vertical. The vast majority\
+    of CSI's economic value resides in our ability to acquire and manage these intangible assets. I can guarantee that\
+    indiscriminately adding back amortisation to create a proxy for earnings available to shareholders will eventually\
+    be misleading. I'd much rather our shareholders focused on the free cash flow that is produced each year and the\
+    trend in that free cash flow.")
+    st.write("We will no longer report ANI and Adjusted EBITA. We will focus instead on IFRS sanctioned measures of\
+    profitability (i.e. net income) and cashflow (i.e. net cashflow from operations) in our MD&A, and will add FCFA2S\
+    as our only non-IFRS metric.")
+    st.write("We calculate FCFA2S by taking net cash flow from operating activities per IFRS, subtracting the amounts that we\
+    spend on fixed assets and on servicing the capital we have sourced from other stakeholders (e.g. debt providers,\
+    lease providers, minority shareholders), and then adding interest and dividends earned on investments. The\
+    remaining FCFA2S is the uncommitted cashflow available to CSI's shareholders if we made no further acquisitions,\
+    nor repaid our other capital-providing stakeholders.")
+    st.write("There has been grumbling from some of CSI's directors and finance staff that I'm creating much ado about nothing…\
+    that FCFA2S is very similar to ANI, so why make the change? Partly, it’s the name. ANI implies income. When\
+    we buy a company that we expect to consistently shrink, then the ANI for that business is actually a combination\
+    of return on capital and return of capital. The name implies otherwise.")
+    st.write("I did like the fact that ANI, unlike CFO, was net of the cost of servicing the capital provided by other stakeholders\
+    (e.g. interest on debt, dividends on preferred stock, and lease payments). The deductions from CFO that we've made\
+    to create FCFA2S reflect this cost. This may become an increasingly important adjustment if we do more partnering\
+    with other investors and operators or create spin-out businesses.")
+    st.write("FCFA2S captures changes in working capital, while ANI did not. If venture-backed competitors use their balance\
+    sheet as a weapon, we will have to respond and that could depress our FCFA2S, but would not depress ANI. At\
+    the extreme, we've seen some venture-backed competitors offering a year or two of free SaaS service in return for\
+    customers signing a 5 year subscription.")
+    st.write("FCFA2S reflects cash taxes, while ANI uses only current taxes. I'm a motivated consumer of corporate tax arcana…\
+    at least when it comes to optimising cash taxes. That said, I am baffled by what the accountants are now doing with\
+    the reported current and deferred tax liabilities and tax assets. I believe that these balance sheet items verge on\
+    meaningless for even the most sophisticated analysts of financial information.")
+    st.write("If you keep a weather eye on the trends in CSI's FCFA2S per share, I think you'll be able to spot any significant\
+    deterioration in the value of our intangible assets.")
     st.write("")
 
 
