adde6f316b08aa1ff955f592141c58dbde4c1773
ZeNoonan <darragh.noonan@gmail.com>
2022-11-03 18:58:37 +0000

    update

--- constellation_software.py
+++ constellation_software.py
@@ -19,6 +19,89 @@ with st.expander('Sep 2021 Q&A'):
     st.write("")
     st.write("")
 
+with st.expander('Aug 2020 Q&A'):
+    st.write("Our Operating Group managers generally check with me before going on an outside board. Some ask me\
+    for leads or ideas regarding finding a directorship. We don’t have a hard and fast policy about outside\
+    board service. My advice to them is that they should serve on a board or two. It can be educational. I\
+    also warn them that the attraction of board membership soon pales unless you have a big chunk of your\
+    net worth invested in the company or are staking your career on the outcome. Only then do you pay\
+    attention, dig in, and fight over the important but frequently unpleasant issues.")
+    
+with st.expander('Feb 2019 Q&A'):
+    st.write("I used 3 ideas to make the case for maintaining hurdle rates to our board this quarter: Hurdle rates are\
+    magnetic, our performance has been exceptional despite (and perhaps because of) our high hurdle rates,\
+    and if we deploy our capital at the “cost of capital” then there are no exceptional returns to share with\
+    employees in the form of bonuses.")
+    st.write("We have only our own experience to go on when it comes to changing hurdle rates. We’ve noticed that\
+    when we increase or drop the hurdle rate, all of the expected returns on our new acquisitions seem to\
+    increase or drop, respectively, in lock step with the new hurdle rates. We summarise this with the phrase\
+    “hurdle rates are magnetic”.")
+    st.write("It is rare to find acquisitive companies that generate high rates of return on invested capital. I did a study\
+    for our managers and directors in the most recent board package. I found a half dozen public companies\
+    out of 4000 had done as well or better than CSI with an acquisition strategy during the last decade. All of\
+    them were operating on a smaller scale than CSI. ")
+    
+with st.expander('Nov 2018 Q&A'):
+    st.write("The biggest failure that pops to mind was taking too much money when we didn’t really need it. We had\
+    one major investor and we were hoping to balance their influence by bringing in another. The second\
+    major investor served that purpose well, but brought challenges of their own. The unnecessary dilution\
+    to our employee shareholders has cost them literally billions of dollars. The experience has made us very\
+    conscious of the value of our shares. We didn’t issue any shares when we went public. We haven’t issued\
+    any in the last decade. Perhaps we have been too sensitive about share issuances because of that early\
+    dilution.")
+
+    st.write("Our second biggest failure was selling one of our vertical market software businesses shortly after we\
+    purchased it. It was a good business. We liked the team. We were offered an extraordinary price for the\
+    business by a venture backed dot‐com. It was early in our history. We were less certain of our abilities\
+    and objectives and took the quick win. I don’t look for quick wins any more. Every now and then, we\
+    check in with the VMS business that we sold. We’d love to buy it back so that we could say that we own\
+    every VMS business that we have ever bought. Our reputation for being a good perpetual owner of VMS\
+    businesses is really important to us… and this is the one glaring example of CSI selling a VMS business.")
+
+    st.write("There is a management maxim that “you learn more from your failures than your successes”. Kahneman\
+    and Tversky’s Prospect Theory (the idea that most humans are risk‐seeking in the domain of probable\
+    losses, and risk‐averse in the domain of probable gains), suggests that people tend to double down on\
+    their losers and cash out their winners. It feels intuitive that learning from large failures is more poignant\
+    than any other experience. But only learning from big mistakes is a slow and painful way to learn. CSI is in\
+    a good position to learn from lots of small experiments and experiences. We are trying to\
+    institutionalise that skill.")
+    
+    st.write("In the 2015 President’s Letter (http://www.csisoftware.com/wp‐content/uploads/2016/04/PL_2015.pdf)\
+    I discussed a CSI study of high performance conglomerates “HPC’s”. Several HPC’s, including Danaher,\
+    had extensive and codified operating methodologies. I don’t doubt that their technique works for the\
+    right kind of organisation.")
+
+    st.write("We do some of the things that the operationally focused HPC’s do: CSI benchmarks its business units and\
+    we share best practices. We usually caveat these practices with phrases like “if you do this, then\
+    historically there’s been a Y% probability that Z will happen”, and “this seems to have worked amongst\
+    X% of our performing businesses”. Because we have hundreds of similar VMS businesses, my sense is that\
+    we are more statistical and less dogmatic about our best practices than the operationally focused HPC’s.\
+    I also have the sense that our best practices are more granular that those found in the HPC’s. Instead of\
+    championing broad processes like lean six sigma or the Pareto principle and applying them to a relatively\
+    small group of businesses competing in different industries, we are more likely to test a very specific\
+    hypothesis with an observational study of several hundred CSI VMS acquisitions. If the hypothesis is\
+    correct, it may lead to the development of a best practice. Whether the hypothesis proves correct or not,\
+    the observational study nearly always leads to a flurry of new hypotheses to test.")
+
+    st.write("A lot of our practices were (and are) the result of ad hoc analysis (see 2016 President’s Letter,\
+    http://www.csisoftware.com/wp‐content/uploads/2017/04/2017‐Presidents‐Letter‐1.pdf). I think we\
+    have less bureaucracy regularly applied to the development, application and compliance with centrally\
+    developed best practices than the operationally focused HPC’s.")
+
+    st.write("CSI has sought, hired, trained and promoted senior managers who both wish and deserve to work\
+    autonomously. The more autonomously you encourage business units to operate, the less likely you are\
+    to be able to specify exactly how they should be operated. Along with Berkshire and ITW (during the\
+    Nichols era), I think we are at the left end of the autonomous vs centrally directed spectrum for the HPC’s.")
+    
+    st.write("You mentioned that there are benefits of a more formal and standardised approach. I’m sure you can\
+    also think of some drawbacks… particularly when you factor in our statistically driven skepticism about\
+    the universality of best practices, our disdain for bureaucracy and our managers’ appetite for autonomy.\
+    We have discussed the idea of gathering and codifying operating methods at the board a couple of times.\
+    The discussions were in‐passing, and not in‐depth.")
+
+
+
+
 with st.expander('April 2018 letter'):
     st.write("Some of our businesses serve shrinking\
     verticals or those that are otherwise troubled, so we don’t necessarily expect strong organic growth from\
@@ -44,7 +127,7 @@ with st.expander('April 2018 letter'):
     in which to invest at attractive prices. I am already casting about for such opportunities. If we don’t find\
     attractive sectors in which to invest, then we’ll return our FCF to our investors. ")
 
-with st.expander('Note to Shareholders regarding non-IFRS Measure'):
+with st.expander('Oct 2019 Q&A: Note to Shareholders regarding non-IFRS Measure'):
     st.write("In my last Letter to Shareholders dated April 20, 2018, I wrote about the pros and cons of using the growth in\
     Adjusted net income (“ANI”) per share versus cash flow from operations ('CFO') per share as indicators of the\
     increase in CSI's economic value. Neither metric is ideal.\
